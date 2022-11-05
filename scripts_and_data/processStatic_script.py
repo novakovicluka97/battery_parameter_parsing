80e5dd8a7414694d763dcb7757aa52e1d8371cca
novakovicluka97 <93382998+novakovicluka97@users.noreply.github.com>
2022-11-05 19:22:24 +0100

    reformating

--- scripts_and_data/processStatic_script.py
+++ scripts_and_data/processStatic_script.py
@@ -42,49 +42,52 @@ def processStatic(static_data, model, typhoon_origin=False):
         if typhoon_origin:
             index_discharge = list(np.where(np.array(static_data[k].script1.current) < 0)[0])
             # First voltage drop when the current starts flowing, step[1] is when the first voltage drop happens
-            IR1Da = static_data[k].script1.voltage[index_discharge[0] - 1] - static_data[k].script1.voltage[index_discharge[0]]
+            I_R0_drop_start = static_data[k].script1.voltage[index_discharge[0] - 1] - static_data[k].script1.voltage[index_discharge[0]]
             # Last voltage drop when the current already charged up the capacitors in the RC circuits
-            IR2Da = static_data[k].script1.voltage[index_discharge[-1]] - static_data[k].script1.voltage[index_discharge[-2]]
+            I_R0_drop_end = static_data[k].script1.voltage[index_discharge[-1]] - static_data[k].script1.voltage[index_discharge[-2]]
             index_charge = list(np.where(np.array(static_data[k].script3.current) > 0)[0])
-            IR1Ca = static_data[k].script2.voltage[-1] - static_data[k].script3.voltage[0]
-            IR2Ca = static_data[k].script3.voltage[-1] - static_data[k].script4.voltage[0]
-            indD = index_discharge
-            indC = index_charge
+            I_R0_rise_start = static_data[k].script2.voltage[-1] - static_data[k].script3.voltage[0]
+            I_R0_rise_end = static_data[k].script3.voltage[-1] - static_data[k].script4.voltage[0]
+            index_discharge = index_discharge
+            index_charge = index_charge
         else:
-            indD  = np.where(np.array(static_data[k].script1.step) == 2)[0]  # index list of all slow discharge
+            index_discharge  = np.where(np.array(static_data[k].script1.step) == 2)[0]  # index list of all slow discharge
             # First voltage drop when the current starts flowing
-            IR1Da = static_data[k].script1.voltage[indD[0] - 1] - static_data[k].script1.voltage[indD[0]]
+            I_R0_drop_start = static_data[k].script1.voltage[index_discharge[0] - 1] - static_data[k].script1.voltage[index_discharge[0]]
             # Last voltage drop when the current already charged up the capacitors in the RC circuits
-            IR2Da = static_data[k].script1.voltage[indD[-1] + 1] - static_data[k].script1.voltage[indD[-1]]
-            indC  = np.where(np.array(static_data[k].script3.step) == 2)[0]  # slow charge
-            IR1Ca = static_data[k].script3.voltage[indC[0]] - static_data[k].script3.voltage[indC[0] - 1]
-            IR2Ca = static_data[k].script3.voltage[indC[-1]] - static_data[k].script3.voltage[indC[-1] + 1]
+            I_R0_drop_end = static_data[k].script1.voltage[index_discharge[-1] + 1] - static_data[k].script1.voltage[index_discharge[-1]]
+            index_charge  = np.where(np.array(static_data[k].script3.step) == 2)[0]  # slow charge
+            I_R0_rise_start = static_data[k].script3.voltage[index_charge[0]] - static_data[k].script3.voltage[index_charge[0] - 1]
+            I_R0_rise_end = static_data[k].script3.voltage[index_charge[-1]] - static_data[k].script3.voltage[index_charge[-1] + 1]
 
-        IR1D = min(IR1Da, 2*IR2Ca)  # For Boulder Colorado data: 0.003254
-        IR2D = min(IR2Da, 2*IR1Ca)  # For Boulder Colorado data: 0.006345
-        IR1C = min(IR1Ca, 2*IR2Da)  # For Boulder Colorado data: 0.012690
-        IR2C = min(IR2Ca, 2*IR1Da)  # For Boulder Colorado data: 0.002928
+        # min of I_R0_rise_end still needs to be understood
+        # todo check for typhoon data if all the R0 drops are the same. They should be
+        IR1D = min(I_R0_drop_start, 2 * I_R0_rise_end)  # For Boulder Colorado data: 0.003254
+        IR2D = min(I_R0_drop_end, 2 * I_R0_rise_start)  # For Boulder Colorado data: 0.006345
+        IR1C = min(I_R0_rise_start, 2 * I_R0_drop_end)  # For Boulder Colorado data: 0.012690
+        IR2C = min(I_R0_rise_end, 2 * I_R0_drop_start)  # For Boulder Colorado data: 0.002928
 
-        blend = np.array(range(len(indD)))/(len(indD)-1)
-        IRblend = IR1D + (IR2D-IR1D)*blend
-        disV = static_data[k].script1.voltage[indD[0]:(indD[-1] + 1)] + IRblend
-        disZ = 1 - static_data[k].script1.disAh[indD[0]:(indD[-1] + 1)] / Q
-        disZ = disZ + (1 - disZ[0])
+        blend = np.array(range(len(index_discharge))) / (len(index_discharge) - 1)  # linear rise from 0 to 1 with len(discharge_index)
+        IR0_blend = IR1D + (IR2D - IR1D) * blend  # IR0_blend exists because R0 measured on end and on beginning aren't the same
+        # and so this list is scaled from R0 beginning to R0 end with this line
+        discharged_voltage_without_IR0 = static_data[k].script1.voltage[index_discharge[0]:(index_discharge[-1] + 1)] + IR0_blend
+        disZ = 1 - static_data[k].script1.disAh[index_discharge[0]:(index_discharge[-1] + 1)] / Q
+        disZ = disZ + (1 - disZ[0])  # small shift in the case of measuring at the sub 100% soc
 
-        blend = np.array(range(len(indC)))/(len(indC)-1)
-        IRblend = IR1C + (IR2C-IR1C)*blend
-        chgV = static_data[k].script3.voltage[indC[0]:indC[-1] + 1] - IRblend
-        chgZ = static_data[k].script3.chgAh[indC[0]:indC[-1] + 1] / Q
+        blend = np.array(range(len(index_charge))) / (len(index_charge) - 1)
+        IR0_blend = IR1C + (IR2C - IR1C) * blend
+        charged_voltage_without_IR0 = static_data[k].script3.voltage[index_charge[0]:index_charge[-1] + 1] - IR0_blend
+        chgZ = static_data[k].script3.chgAh[index_charge[0]:index_charge[-1] + 1] / Q
         chgZ = chgZ - chgZ[0]
 
-        Voltage_SOC_curve = interpolate.interp1d(chgZ, chgV)
-        Discharge_SOC_curve = interpolate.interp1d(disZ, disV)
+        Voltage_SOC_curve = interpolate.interp1d(chgZ, charged_voltage_without_IR0)
+        Discharge_SOC_curve = interpolate.interp1d(disZ, discharged_voltage_without_IR0)
         deltaV50 = Voltage_SOC_curve(0.5) - Discharge_SOC_curve(0.5)
         ind = np.where(chgZ < 0.5)[0]
-        vChg = chgV[ind[0]:ind[-1]] - chgZ[ind[0]:ind[-1]]*deltaV50
+        vChg = charged_voltage_without_IR0[ind[0]:ind[-1]] - chgZ[ind[0]:ind[-1]] * deltaV50
         zChg = chgZ[ind[0]:ind[-1]]
         ind = np.where(disZ > 0.5)[0]
-        vDis = np.flipud(disV[ind[0]:ind[-1]] + (1 - disZ[ind[0]:ind[-1]])*deltaV50)
+        vDis = np.flipud(discharged_voltage_without_IR0[ind[0]:ind[-1]] + (1 - disZ[ind[0]:ind[-1]]) * deltaV50)
         zDis = np.flipud(disZ[ind[0]:ind[-1]])
         np.append(zChg, zDis)
         FULL_SOC_curve = interpolate.interp1d(np.append(zChg, zDis), np.append(vChg, vDis), fill_value="extrapolate")
